4468c1827d9dce8395405df1204e41f03dc25f75
Peter Post <peter.post@hva.nl>
2019-04-27 09:44:08 +0200

    eerste opzet repo

---
+++ src/main/java/com/team/blanc/MijnInzet/MijnInzetApplication.java
@@ -0,0 +1,13 @@
+package com.team.blanc.MijnInzet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijnInzetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijnInzetApplication.class, args);
+	}
+
+}
