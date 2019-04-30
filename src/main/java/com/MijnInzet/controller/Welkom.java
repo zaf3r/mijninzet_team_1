b4f0bf1bc98945877c10ffd05c5ecd6d52c51c2e
Peter Post <peter.post@hva.nl>
2019-04-30 15:26:11 +0200

    vorige wijzigen committen

--- src/main/java/com/MijnInzet/controller/Welkom.java
+++ src/main/java/com/MijnInzet/controller/Welkom.java
@@ -7,7 +7,19 @@
 package com.MijnInzet.controller;
 
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class Welkom {
+
+    @RequestMapping("/hello")
+    String home() {
+        return "Welkom leden team 1 van cohort 15 Make It Work";
+    }
+
 
 
 }
